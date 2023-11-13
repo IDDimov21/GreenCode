352c52c2d930e6456db2f0bf3db84024c87ed81b
IDDimov21 <IDDimov21@codingburgas.bg>
2023-11-13 19:24:39 +0200

    Make HARD functions + organise + almost finish up hard level + scroll/idle assets

--- GreenCode/GreenCode/gameStructures.cpp
+++ GreenCode/GreenCode/gameStructures.cpp
@@ -3,13 +3,18 @@
 
 
 void DragNDrop(bool& collision, bool& isDragging, bool& isSnapped, bool& check1, Rectangle& Option, Rectangle& OptionSlot1, Rectangle& OptionSlot2) {
+    static Rectangle* currentDraggedOption = nullptr;
+
     if (collision) {
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(GetMousePosition(), Option)) {
             isDragging = true;
             isSnapped = false;
+            currentDraggedOption = &Option;
         }
         if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
             isDragging = false;
+            currentDraggedOption = nullptr;
+
             if (CheckCollisionRecs(Option, OptionSlot1)) {
                 Option.x = OptionSlot1.x + 14;
                 Option.y = OptionSlot1.y + 2;
@@ -23,7 +28,7 @@ void DragNDrop(bool& collision, bool& isDragging, bool& isSnapped, bool& check1,
                 check1 = true;
             }
         }
-        if (isDragging) {
+        if (isDragging && currentDraggedOption == &Option) {
             Option.x = GetMouseX() - Option.width / 2;
             Option.y = GetMouseY() - Option.height / 2;
             isSnapped = false;
